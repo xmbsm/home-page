86a87e2b6c385b2386b4b5465a5f2a760634e9ca
xmbsm <21903538@qq.com>
2026-05-01 21:52:22 +0800

    提交

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import NavBar from "./NavBar";
-import Welcome from "#components/Welcome";
-import Dock from "./Dock";
-import WindowControls from "./WindowControls";
-import Home from "./Home";
-import Clock from "./Clock";
-
-export { NavBar, Welcome, Dock, WindowControls, Home, Clock}
--- src/store/theme.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-const useThemeStore = create((set) => ({
-  isDarkMode: false,
-  toggleTheme: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
-  setDarkMode: (isDark) => set({ isDarkMode: isDark }),
-}));
-
-export default useThemeStore;
